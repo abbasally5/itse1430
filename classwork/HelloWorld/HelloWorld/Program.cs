f50d1c354d040dc17cbbf344c4b0a2246568dfe0
abbasally5 <abbasally5@yahoo.com>
2020-08-26 21:35:22 -0500

    Playing around with C#

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -6,22 +6,50 @@ using System;
 
 namespace HelloWorld
 {
+    // Pascal casing - Capitalize on wod boundaries including first word
+    // Camel casing - Capitalize on word boundaries except first (e.g. firstName, payRate)
     class Program
     {
-        static void Main(string[] args)
+        // Function declaration - declaration to compiler that a function exists with a given signature
+        //Function signature = function name and the parameter types (sometimes it will include return type)
+        // Function definition - declaration + implementation
+        static void Main(string[] args) // 1 parameter
         {
             // a single line comment
             // Another line of comments
-            Console.WriteLine("Hello World!");
 
-            int hours = 40;
-            double payRate = 12.80;
+            //Display given string to output
+            //Arguments - data passed to function
+            Console.WriteLine("Hello World!"); // printf, cout
 
-            double totalPay = hours * payRate;
+            //Variable declaration:
+            // T id;
+            //int hours;
+            //hours = 10; //Assignment operator: id = E;
 
-            // Another block
-            // 10 + 45;
-            // NO!!! { Console.WriteLine(); }
+            //Identifier Rules
+            // 1. Unique within scope
+            // 2. Must start with underscore or letter
+            // 3. Consist of letters, digits, and underscores
+            // ex.
+            //  Valid? firstname - yes
+            //  pay1Rate - yes
+            //  1chance - no
+
+            //Always camel case local variables
+            //Preferred: T id = E;
+            int hours = 10;
+
+            //int pay = 0;
+            //pay = hours * 9;
+            int totalPay = hours * 9;
+
+            //function overloading - multiple functions with same name but different parameters
+            // atof - converts string to double
+            // atoi - converts string to integer
+            // c did not start off with function overloading so it had to make different named functions for similar functionality
+            Console.WriteLine(totalPay);
+
         }
     }
 }
