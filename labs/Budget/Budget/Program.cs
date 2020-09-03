5176058a4e449214d35e3e7e098e73562ff8614e
Abbas Ally <abbasally5@yahoo.com>
2020-09-02 21:42:35 -0500

    working on movie library project in class

---
+++ labs/Budget/Budget/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Abbas Ally
+ * ITSE 1430
+ * Lab 1
+ */
+
+using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is budget program written by Abbas Ally");
+        }
+    }
+}
